ad4ebab51b6892db7a26fb8fac086967fdcf3060
Nabeela1 <hamidnabeela@gmail.com>
2015-02-11 12:10:38 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/admin/utest/Tests/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.admin.utest.Tests;
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.admin.utest.MainActivity;
+
+import junit.framework.Assert;
+
+/**
+ * Created by Admin on 2015-01-29.
+ */
+public class MainActivityTest {
+public MainActivityTest() {
+
+
+}
+        }
